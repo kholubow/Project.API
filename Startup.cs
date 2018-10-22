d736911c5ff0ebfaab04c4fc7d8b5eb74d95ac63
GCoder10 <https://github.com/GCoder10/Web-api.git>
2018-10-23 00:13:32 +0200

    commit
    commit

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace Project.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +37,7 @@ namespace Project.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
